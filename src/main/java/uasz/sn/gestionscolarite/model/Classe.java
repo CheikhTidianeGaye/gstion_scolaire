47a88f02aeb3e4badb9d17faf96d242826c160bd
FAYE <o.f14@zig.univ.sn>
2025-02-24 13:03:43 +0000

    modification du model class.java

--- src/main/java/uasz/sn/gestionscolarite/model/Classe.java
+++ src/main/java/uasz/sn/gestionscolarite/model/Classe.java
@@ -1,4 +1,15 @@
 package uasz.sn.gestionscolarite.model;
 
+@Entity
 public class Classe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String niveau;
+
+    @OneToMany(mappedBy = "classe")
+    private List<Eleve> eleves;
+
+    // Getters et Setters
 }
